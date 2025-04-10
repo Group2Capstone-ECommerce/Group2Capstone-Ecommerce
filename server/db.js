a3c50c5084eaef43a78d0d8fb6a6d7b84b59757f
apelic <apelic92@gmail.com>
2025-04-10 09:30:59 -0500

    added products and users tables

--- server/db.js
+++ server/db.js
@@ -19,6 +19,43 @@ const connectDB = async () => {
 };
 
 const createTables = async () => {
+  try {
+    console.log("Creating tables...");
+
+    // UUID generation function
+    await client.query(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`);
+
+    //Create products
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS products (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        product_name VARCHAR(100) NOT NULL,
+        descriptions TEXT,
+        price NUMERIC(10, 2) NOT NULL,
+        tags TEXT[],
+        image_urls TEXT[],
+        rating NUMERIC(2, 1) CHECK (rating >= 0 AND rating <= 5) DEFAULT 5,
+        stock_quantity INTEGER DEFAULT 0,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+
+    //Create users
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        is_admin BOOLEAN DEFAULT FALSE,
+        mailing_address TEXT,
+        phone VARCHAR(20),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+  }
 }
 
 module.exports = {
