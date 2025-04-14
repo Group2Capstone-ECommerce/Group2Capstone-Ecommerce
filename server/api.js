43f8f265feb66c72581bc5d2b0027fc8edcf22fe
OguKeith123 <keithogu1@gmail.com>
2025-04-14 11:22:54 -0400

    API changes made.

--- server/api.js
+++ server/api.js
@@ -4,7 +4,7 @@ const client = require("./db.js").client;
 
 router.get("/products", async (req, res) => {
   try {
-    const result = await client.query("SELECT * FROM products;");
+    const result = await client.query;
     res.json(result.rows); // This should return products as a JSON array
   } catch (error) {
     console.error("Error fetching products:", error);
