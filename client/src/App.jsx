22563ffc3bc51ee4ef3bfe3f7b1b9ec714067815
Megan Chiu <megan.chiu.2014@gmail.com>
2025-04-22 10:15:53 -0500

    pr fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import ProductList from './components/ProductList'
 import ProductDetails from './components/ProductDetails'
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const PRODUCTS_API_URL = "http://localhost:3000/api/products";
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(PRODUCTS_API_URL);
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching all products.");
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<ProductList />} />
+        <Route path='/' element={<ProductList products={products} fetchProducts={fetchProducts} />} />
         <Route path='/products/:productId' element={<ProductDetails />} />
       </Routes>
     </>
--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,48 +1,32 @@
 // Page to view all products available for purchase
 
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import stockImage from '../assets/stockProductImg.png';
 
-export default function ProductList() {
-  const [products, setProducts] = useState([]);
-  const PRODUCTS_API_URL = "http://localhost:3000/api/products";
-  
-  useEffect(() => {
-    const getProducts = async() => {
-      try {
-        const response = await fetch(PRODUCTS_API_URL);
-        const data = await response.json();
-        console.log(`data => `, data);
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching all products.");
-      }
-    };
-    getProducts();
-  }, [])
-  
+export default function ProductList({ products, fetchProducts }) {
   return (
-    <>
-      <div className="productsContainer">
+    <div className="productsContainer">
       {products.length === 0 ? (
         <p>Loading products...</p>
       ) : (
         products.map((product) => (
           <div key={product.id} id={product.id} className="productCard">
-              <h3>Product Name: {product.product_name}</h3>
-              <img
-                src={product.image_url || stockImage}
-                alt={product.product_name}
-              />
-              {product.is_available ? <p id="productAvailableTxt">Available</p> : <p id="productNotAvailableTxt">Not Available</p>}
-              <Link to={`/products/${product.id}`}>
+            <h3>Product Name: {product.product_name}</h3>
+            <img
+              src={product.image_url || stockImage}
+              alt={product.product_name}
+            />
+            {product.is_available ? (
+              <p id="productAvailableTxt">Available</p>
+            ) : (
+              <p id="productNotAvailableTxt">Not Available</p>
+            )}
+            <Link to={`/products/${product.id}`}>
               <button className="seeDetailsBtn">See Details</button>
-              </Link>
+            </Link>
           </div>
         ))
       )}
-      </div>
-    </>
-  )
-};
+    </div>
+  );
+}
