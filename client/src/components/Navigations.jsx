49d57376438ce05baa94d31596349ecd054058a4
Megan Chiu <megan.chiu.2014@gmail.com>
2025-04-23 11:32:43 -0500

    basic navigation router

---
+++ client/src/components/Navigations.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export default function Navigations() {
+  return (
+    <>
+      <div id="navBar">
+        <div>
+          <Link to="/">Products</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
+    </>
+  )
+};
