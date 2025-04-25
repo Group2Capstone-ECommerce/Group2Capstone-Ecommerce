4299b8ce41ab51a552d3868afdb9d67eae570445
apelic <apelic92@gmail.com>
2025-04-25 12:35:48 -0500

    added tabs to admin dashboard

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import { useAuth } from "../components/AuthContext.jsx";
+
 export default function AdminDashboard() {
+  const { isAdmin, setIsAdmin } = useAuth();
+  const tabs = ["Dashboard", "View Users", "Add/Edit Products"];
+  const [activeTab, setActiveTab] = useState("Dashboard");
+
   return (
     <>
-      <h1>THIS IS THE ADMIN DASHBOARD</h1>
+      <div className="admin-container">
+        <h1 className="admin-title">Admin Dashboard</h1>
+
+        {!isAdmin ? (
+          <p className="access-denied">Access Denied. Admins Only.</p>
+        ) : (
+          <>
+          {/*Tab Buttons*/}
+          <div className="tab-buttons">
+            {tabs.map((tab) => (
+              <button
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={`tab-button ${activeTab === tab ? "active" : ""}`}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+
+          {/*Tab Content*/}
+          <div className="tab-content">
+            {activeTab === "Dashboard" && <p>Welcome Admin!</p>}
+            {activeTab === "View Users" && <p>All Users:</p>}
+            {activeTab === "Add/Edit Products" && <p>Add/Edit Products:</p>}
+          </div>
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
