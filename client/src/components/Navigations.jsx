15af011e10d0645546ded7c2d61f57bcf87e6791
Yue Xie <moonxie9304@gmail.com>
2025-05-06 16:43:27 -0500

    fixed bug to get isAdmin value

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -1,9 +1,38 @@
 import { Link } from "react-router-dom"
 import { useAuth } from "../components/AuthContext.jsx";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 export default function Navigations() {
-  const {token, setToken} = useAuth();
-  const {isAdmin, setIsAdmin} = useAuth();
+  const {token} = useAuth();
+  const {isAdmin, setIsAdmin} = useAuth()
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/users/me', {
+          method:'GET',
+          headers:{
+              Authorization: `Bearer ${token}`,
+          },
+      });
+        const result = await response.json();
+        console.log('fetch user result =>', result);
+        if (!response.ok) {
+          return toast.error('No user found!');
+        }
+        setIsAdmin(result.is_admin); //Set admin status
+      } catch (error) {
+        toast.error('Something went wrong when fetching user!');
+      }
+    };
+
+    if (token) {
+      fetchUser();
+    }
+  }, [token, setIsAdmin]); //Only runs when token changes
+  console.log('token is =>', token)
+  console.log('Is admin: =>', isAdmin)
 
   return (
     <>
